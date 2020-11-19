160ae0fd6fa7151f3c825c10c202fedb9c7e6c55
P900017 <mauricio.meza@sura.pe>
2020-11-18 19:41:11 -0500

    version inicial montecarlo mau

---
+++ montecarlo.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Importo datos
+file_name = "Contratos"
+# main_path = ("F:\\Inversiones\\VP_Inversiones\\SIM\\Research\\2. Top down\\Commodities\\Backtesting\\")
+file_path = (file_name + ".xlsx")
+sheet_name = "Datos Diarios BBG"
+
+df = pd.read_excel(file_path, sheet_name = sheet_name, parse_dates = True, index_col = 0)
+
+#Selecciono los nodos curvas de los commodities
+selected_tickers = ['CO1 Comdty', 'CO2 Comdty', 'CO3 Comdty', 'CO4 Comdty', 'CO5 Comdty', 'HG1 COMB Comdty', 'HG2 COMB Comdty', 'HG3 COMB Comdty', 'HG4 COMB Comdty',
+                   'HG5 COMB Comdty', 'GC1 COMB Comdty', 'GC2 COMB Comdty', 'GC3 COMB Comdty', 'GC4 COMB Comdty', 'GC5 COMB Comdty']
+df = df.loc[:, selected_tickers].dropna()
+
+retorno_oil=df_oil.pct_change()
+retorno_copper=df_copper.pct_change()
+retorno_gold=df_gold.pct_change()
+col_oil=len(retorno_oil.columns)-1
+col_copper=len(retorno_copper.columns)-1
+col_gold=len(retorno_gold.columns)-1
+
+n_iter = 10000
+
+for i in range(n_iter):
+
+    # escogiendo contratos 
+    oil_ct1 = int(np.random.randn()*col_oil)
+    oil_ct2 = int(np.random.randn() * col_oil)
+    while oil_ct2 >= oil_ct1:
+        oil_ct2 = int(np.random.randn() * col_oil)
+
+    copper_ct1 = int(np.random.randn() * col_copper)
+    copper_ct2 = int(np.random.randn() * col_copper)
+    while copper_ct2 >= copper_ct1:
+        copper_ct2 = int(np.random.randn() * col_copper)
+
+    gold_ct1 = int(np.random.randn() * col_gold)
+    gold_ct2 = int(np.random.randn() * col_gold)
+    while gold_ct2 >= gold_ct1:
+        gold_ct2 = int(np.random.randn() * col_gold)
+
+    # creando retornos
+    oil_ret = df_oil[df_oil.columns[oil_ct1]]/df_oil[df_oil.columns[oil_ct1]]-1
+    copper_ret = df_oil[df_oil.columns[copper_ct1]]/df_oil[df_oil.columns[copper_ct1]]-1
+    gold_ret = df_oil[df_oil.columns[gold_ct1]]/df_oil[df_oil.columns[gold_ct2]]-1
+
+    print("Contratos Oil " + str(oil_ct1) + "/" + str(oil_ct2))
+    print("Contratos Copper " + str(copper_ct1) + "/" + str(copper_ct2))
+    print("Contratos Gold " + str(gold_ct1) + "/" + str(gold_ct2))
